00dd8676aa2daccf63c8aa7b868d5b628094afb9
kyrillosashraf410 <kyrillosashraf410@gmail.com>
2026-09-16 21:59:35 +0300

    ss

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
 var buttons = document.querySelectorAll(".btn");
 var screen = document.querySelector(".screen");
+// buttons.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     if (btn.innerHTML == "Egypt") {
+//       screen.children[0].src = "Egyptpng.png";
+//     } else if (btn.innerHTML == "palstan") {
+//       screen.children[0].src = "palasten.png";
+//     } else if (btn.innerHTML == "moroco") {
+//       screen.children[0].src = "moroco.png";
+//     } else if (btn.innerHTML == "DeleteCountry") {
+//       screen.children[0].src = "";
+//     }
+//   });
+// });
+
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (btn.innerHTML == "Egypt") {
-      screen.children[0].src = "Egyptpng.png";
-    } else if (btn.innerHTML == "palstan") {
-      screen.children[0].src = "palasten.png";
-    } else if (btn.innerHTML == "moroco") {
-      screen.children[0].src = "moroco.png";
-    }
+    var $masar = btn.getAttribute("data-img");
+    screen.children[0].src = $masar;
   });
 });
