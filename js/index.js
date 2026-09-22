9227cbb69ad4cf7d065a9b28979f746ce72ab600
kyrillosashraf410 <kyrillosashraf410@gmail.com>
2026-09-22 14:22:14 +0300

    tooo

--- js/index.js
+++ js/index.js
@@ -1,22 +1,19 @@
 var buttons = document.querySelectorAll(".btn");
-var screen = document.querySelector(".screen");
-// buttons.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     if (btn.innerHTML == "Egypt") {
-//       screen.children[0].src = "Egyptpng.png";
-//     } else if (btn.innerHTML == "palstan") {
-//       screen.children[0].src = "palasten.png";
-//     } else if (btn.innerHTML == "moroco") {
-//       screen.children[0].src = "moroco.png";
-//     } else if (btn.innerHTML == "DeleteCountry") {
-//       screen.children[0].src = "";
-//     }
-//   });
-// });
+var screen = document.querySelectorAll(".city");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    var $masar = btn.getAttribute("data-img");
-    screen.children[0].src = $masar;
+    screen.forEach((city) => city.classList.add("d-none"));
+
+    const $getId = btn.getAttribute("data-content");
+    if ($getId === "delete") {
+      return;
+    }
+
+    const $selectCity = document.getElementById($getId);
+
+    if ($selectCity) {
+      $selectCity.classList.remove("d-none");
+    }
   });
 });
